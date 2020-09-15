655f6911146874147384f1210120eb83d33525e8
akshay-gujalwar <akshaygujalwar96@gmail.com>
2020-09-15 09:03:50 +0530

    added comment

--- src/main/java/guru/springframework/model/User.java
+++ src/main/java/guru/springframework/model/User.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class User {
-
+    //used lombok
     private String firstName;
     private String lastName;
     private String email;
